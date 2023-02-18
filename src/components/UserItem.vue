2f327a02442099893dd6ea288a2c2f399b019692
Alan <alan.jordy1999@gmail.com>
2023-02-18 17:21:37 +0100

    🥳 initial commit

---
+++ src/components/UserItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <span >Hello {{ user.name }}</span>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  user: {
+    id: Number,
+    name: String
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/UserList.vue
@@ -0,0 +1,66 @@
+<script setup>
+import UserItem from './UserItem.vue'
+import {reactive} from "vue";
+
+const users = reactive([
+  {
+    id: 1,
+    name: "Alan"
+  },
+  {
+    id: 2,
+    name:"Theo"
+  },
+  {
+    id: 2,
+    name:"Theo"
+  },
+  {
+    id: 3,
+    name:"Mansour"
+  },
+  {
+    id: 4,
+    name:"Arnaud"
+  }
+  ])
+</script>
+<template>
+  <div class="right-panel">
+    <h3>Utilisateurs</h3>
+    <ul>
+      <li v-for="(_,index) of users"><UserItem :user="users[index]"></UserItem></li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+.right-panel {
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  width: 250px;
+  background-color: #2a2d31;
+}
+.right-panel h3 {
+  text-align: center;
+  border-radius: 5px;
+  box-shadow: 1px 0 5px 1px #252525;
+  margin-top: 3px;
+  margin-left: 2px;
+  margin-bottom: 20px;
+  padding: 2px;
+  background-color: #303338;
+
+}
+.right-panel ul {
+  list-style: url('../assets/logo.svg');
+  text-align: left;
+
+}
+.right-panel li {
+  margin-top: 5px;
+
+}
+</style>
