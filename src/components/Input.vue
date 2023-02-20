f41ef932715f5f51a6038bccf412fcbac56443c9
MansourWolou <Wolou0444@gmail.com>
2023-02-20 14:44:03 +0100

    input done

---
+++ src/components/Input.vue
@@ -0,0 +1,22 @@
+<script>
+// ce composant correspond à la zone de saisi pour un chanel
+</script>
+
+<template>
+    <input type="text" id="input" name="input" placeholder="Envoyer un message dans 'nom du channel'">
+</template>
+
+<style scoped>
+input {
+    width: 80%;
+    padding: 1em;
+    /* margin: 1em; */
+    margin-left: 1%;
+    margin-right: 1%;
+    margin-top: 45%;
+    margin-bottom: 1%;
+    background-color: gainsboro;
+    border-radius: 18px;
+
+}
+</style>
