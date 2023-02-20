c50f65a8cf12dbc50ac6af343453bb331908c0b1
Totba <theodesmoulins@gmail.com>
2023-02-20 14:40:16 +0100

    🫡 Ez

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -2,15 +2,39 @@
 <div class="login-panel">
   <h1>Login</h1>
   <label for="user">Nom d'utilisateur :</label>
-  <input id="user" type="text"><br>
+  <input v-model="username" placeholder="Username" type="text" id="user"><br>
   <label for="password">Mots de passe :</label>
-  <input id="password" type="password"><br>
-  <button>Log In</button>
+  <input v-model="password" placeholder="password" type="password" id="password"><br>
+  <button v-on:click="login">Log In</button>
 </div>
 </template>
 
-<script setup>
-
+<script>
+import {ServiceUtilisateur} from "../service/ServiceUser";
+export default {
+    data: () => ({
+        username: "",
+        password: "",
+        error: ""
+        }),
+    methods: {
+        async login(){
+          console.log("username", this.username)
+          console.log("password", this.password)
+            if(this.username.trim() !== "" && this.password.trim() !== "") {
+            ServiceUtilisateur.login(this.username, this.password)
+            .then(async (response) => {
+                const result = await response.json();
+                if(response.status === 200) {
+                localStorage.setItem("token", result.token)
+                } else {
+                this.error = result.message;
+                }
+            });
+            }
+        }
+    }
+}
 </script>
 
 <style scoped>
