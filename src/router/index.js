a5dd12200e0473d7ebcad57f5ff9650cce3b393c
Alan <alan.jordy1999@gmail.com>
2023-02-23 20:20:08 +0100

    :lock: Add security on route

--- src/router/index.js
+++ src/router/index.js
@@ -8,17 +8,31 @@ const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
-        path: '/',
-        name: 'login',
-        component: LoginView
+            path: '/',
+            name: 'login',
+            component: LoginView,
+            beforeEnter: (to, from) => {
+                if(localStorage.getItem('token') == null){
+                    return true;
+                }
+                else{
+                    router.push("/channels");
+                }
+            },
         },
         {
-        path: '/channels',
-        name: 'channels',
-        // route level code-splitting
-        // this generates a separate chunk (About.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: HomePage
+            path: '/channels',
+            name: 'channels',
+            // route level code-splitting
+            // this generates a separate chunk (About.[hash].js) for this route
+            // which is lazy-loaded when the route is visited.
+            component: HomePage,
+            beforeEnter: (to, from) => {
+                // reject the navigation
+                if(localStorage.getItem('token') == null){
+                    router.replace("/");
+                }
+            },
         },
         {
             path: '/channels/:id',
@@ -26,7 +40,13 @@ const router = createRouter({
             // route level code-splitting
             // this generates a separate chunk (About.[hash].js) for this route
             // which is lazy-loaded when the route is visited.
-            component: HomePage
+            component: HomePage,
+            beforeEnter: (to, from) => {
+                // reject the navigation
+                if(localStorage.getItem('token') == null){
+                    router.replace("/");
+                }
+            },
         },
         {
         path: "/:pathMatch(.*)*",
